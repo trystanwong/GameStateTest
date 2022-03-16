ceb11cf3ab2b4ea5f3c31e90515b2e483938bc8f
marcus <marcusrison.21@gmail.com>
2022-03-15 19:57:36 -0700

    added forfeit text and choose ante text

--- app/src/main/java/com/example/gamestatetest/MainActivity.java
+++ app/src/main/java/com/example/gamestatetest/MainActivity.java
@@ -33,16 +33,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         GameState firstInstance = new GameState();
         GameState firstCopy = new GameState(firstInstance);
+        GameState secondInstance = new GameState();
+        GameState secondCopy = new GameState(secondInstance);
         int currentPlayer = firstInstance.getCurrentPlayer();
 
         //playing the first card in the current players hand.
         boolean endTurn = firstInstance.endTurn(currentPlayer);
         EditText line = findViewById(R.id.simple);
-        line.setText(firstCopy.toString(), TextView.BufferType.SPANNABLE);
+        line.setText(firstCopy.toString());
         boolean playCard = firstInstance.playCard(currentPlayer,
                 firstInstance.getCard(currentPlayer,0));
         boolean selectCard = firstInstance.selectCard();
         boolean chooseAnte = firstInstance.chooseAnte(currentPlayer);
         boolean forfeit = firstInstance.forfeit();
+        EditText forfeitText = findViewById(R.id.forfeitText);
+        forfeitText.setText("Player 0 got tired of Player 1's tactics and rage quited", TextView.BufferType.SPANNABLE);
+        EditText chooseAnteText = findViewById(R.id.chooseAnteText);
+        chooseAnteText.setText("Player 0 chose the red dragon from the ante pile.");
+
+
     }
 }
